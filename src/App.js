c83777ef5d2a5f65613d1190b57b324013650f9d
Atharva Vibhute <94898067+atharvavibe@users.noreply.github.com>
2023-07-09 12:22:14 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import ExpenseItem from "./components/Expenses/ExpenseItem";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expense = [
@@ -12,7 +13,7 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense/>
       <Expenses items={expense}/>
     </div>
   );
