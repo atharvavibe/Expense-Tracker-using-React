36a3aac2a7650544bf8a33a58dff4f1638e51886
Atharva Vibhute <94898067+atharvavibe@users.noreply.github.com>
2023-07-12 10:20:58 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
+
+import React, { useState } from 'react';
+
 import ExpenseItem from "./components/Expenses/ExpenseItem";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  { id:'1e', title: "Car Insurance", amount: 299.23, date: new Date(2021, 7, 13) },
+  { id:'2f', title: "Ramen", amount: 20.23, date: new Date(2022, 3, 29) },
+  { id:'3g', title: "Manga", amount: 15.23, date: new Date(2021, 6, 3) },
+  { id:'5i', title: "MacD", amount: 5.23, date: new Date(2021, 12, 18) },
+];
+
 function App() {
-  const expense = [
-    { title: "Car Insurance", amount: 299.23, date: new Date(2021, 7, 13) },
-    { title: "Ramen", amount: 20.23, date: new Date(2022, 3, 29) },
-    { title: "Manga", amount: 15.23, date: new Date(2021, 6, 3) },
-    { title: "MacD", amount: 5.23, date: new Date(2021, 12, 18) },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    })
+
   }
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler}/>
-      <Expenses items={expense}/>
+      <Expenses items={expenses}/>
     </div>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,6 +5,13 @@ import Card from '../UI/Card';
 function Expenses(props) {
   return (
     <Card className="expenses">
+      {props.items.map(expense => <ExpenseItem 
+       key={expense.id}
+       title={expense.title}
+       amount={expense.amount}
+       date = {expense.date}/>)
+       }
+
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
